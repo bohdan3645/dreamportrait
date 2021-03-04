b2dd115a57b1c4ef9cc0a199b6108f6793c1a5da
VasylRishko <rishko92@gmail.com>
2021-03-04 14:34:01 +0200

    Add success page.

--- routes/cartPage.js
+++ routes/cartPage.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET cart page. */
 router.get('/', function(req, res, next) {
    res.render('user/cartPage', { title: 'Dream Portrait', isDevelopment: process.env.IS_DEVELOPMENT });
 });
--- routes/successPay.js
+++ routes/successPay.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-
-/* GET successPay page. */
-router.get('/', function(req, res, next) {
-   res.render('successPages/successPay', { title: 'Dream Portrait' });
+router.get('/', (req, res, next) => {
+   res.render('successPages/successPay');
 });
 
 module.exports = router;
